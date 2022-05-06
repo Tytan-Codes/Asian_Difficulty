20f38668fe1e192b054093f5a4fc402c3eb21af4
Tysudo <91703892+tysudo@users.noreply.github.com>
2022-05-06 16:46:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -42,22 +42,28 @@ def hard():
     print('What a disappointment')
     
 def ASIAN():
-    asian = str(input('A leaf is falling from the tree. What do you do? '))
+    os.system('cls')
     print('(1) Run')
     print('(2) Hide')
     print('(3) Stay where you are')
     print('(4) Call 911')
+    asian = str(input('A leaf is falling from the tree. What do you do? '))
+    
     
     if asian == '1':
+        os.system('cls')
         print('You tripped on a branch and you DIED ')
     
     if asian == '2':
-        print('You died from low blood in your brain ')
+        os.system('cls')
+        print('You died from no blood to your brain ')
     
     if asian == '3':
+        os.system('cls')
         print('The leaf hit you, but you did not know that there was poison on the leaf. The poison killed you')
     
     if asian == '4':
+        os.system('cls')
         print('The police sentenced you to death from prank calling')
 # MODES ENDS eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 
