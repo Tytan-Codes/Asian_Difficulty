abf7b57cc1299479839f645006534754ad91abfc
Tysudo <91703892+tysudo@users.noreply.github.com>
2022-05-06 16:27:32 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+
+def main():
+    print('(1) Easy mode')
+    print('(2) Medium mode')
+    print('(3) Hard mode ') 
+    print('(4) Asian mode')
+    
+    main = (int(input('What mode do you want to play: ')))
+    
+    if main == 1:
+        easy()
+    
+       
+# MODES eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+
+
+def easy():
+    print('What a disaster')
+    
+    
+main()
+
+
+# MODES ENDS eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+    
+    
